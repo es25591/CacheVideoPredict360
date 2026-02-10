e3312d8514bebd27d619ca2c821c1039a7955bf7
eduardo <eduardogama72@gmail.com>
2026-02-10 00:27:05 +0000

    Modifying simulation parameters ]incomplete#

--- Sources/Common/config.py
+++ Sources/Common/config.py
@@ -1,12 +1,12 @@
 # --- 1. CONFIGURATION & HYPERPARAMETERS (Section VII-B) ---
 class Config:
-    n_episodes: int = 500
+    n_episodes: int = 400
     max_steps: int = 10000
     n_nodes: int = 3
     n_users: int = 500
     step_size: float = 10.0
     arrival_rate: float = 200.0  # users per second
-    zipf_alpha: float = 0.5
+    zipf_alpha: float = 0.8
     n_videos: int = 500
     n_gops: int = 30
     n_layers: int = 2
@@ -36,7 +36,7 @@ class Config:
     gamma: float = 0.6
     batch_size: int = 32
     buffer_capacity: int = 2000
-    nb_interval: int = 100  # interval to update target network
+    nb_interval: int = 200  # interval to update target network
 
     h_short: int = 300   # sliding windows for popularity (Section VI-A)
     h_long: int = 1000
@@ -47,13 +47,13 @@ class Config:
 
     # Paths for data and results
 
-    # path_data: str = '/home/eduardo/Workspace/CacheVideoPredict360/Data'
-    # path_results: str = '/home/eduardo/Workspace/CacheVideoPredict360/Results'
-    # path_trajectories: str = '/home/eduardo/Workspace/CacheVideoPredict360/Dataset/Trajectories'
+    path_data: str = '/home/eduardo/Workspace/CacheVideoPredict360/Data'
+    path_results: str = '/home/eduardo/Workspace/CacheVideoPredict360/Results'
+    path_trajectories: str = '/home/eduardo/Workspace/CacheVideoPredict360/Dataset/Trajectories'
 
-    path_data: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Data'
-    path_results: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Results'
-    path_trajectories: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Dataset\Trajectories'
+    # path_data: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Data'
+    # path_results: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Results'
+    # path_trajectories: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Dataset\Trajectories'
 
     filename: str = f"drl_dqn_lrdecay{learning_rate_decay}_c{cache_size}_ar{arrival_rate}_z{zipf_alpha}.csv"
     
