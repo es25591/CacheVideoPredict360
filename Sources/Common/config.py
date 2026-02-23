62e8308745df16ec8c421b6cb507d4b89f6eed39
De Souza Gama <es25591@essex.ac.uk>
2026-02-23 15:41:21 +0000

    Commit with the PPO env [incomplete]

--- Sources/Common/config.py
+++ Sources/Common/config.py
@@ -11,6 +11,7 @@ class Config:
     zipf_alpha: float = 0.8
     n_videos: int = 500
     n_gops: int = 30
+    user_session_length: int = 60
     n_layers: int = 2
     n: int = 4
     m: int = 3
@@ -29,19 +30,20 @@ class Config:
     # Hyperparameters for RL
     epsilon_start: float = 1.0
     epsilon_min: float = 0.05
-    epsilon_decay: float = 0.987 # (epsilon_min / epsilon_start) ** (1.0 / n_episodes)
+    epsilon_decay: float = 0.98
     entropy_coef: float = 0.02
 
     learning_rate: float = 1e-3
-    learning_rate_decay: float = 0.9999
+    learning_rate_decay: float = 0.9995
 
     tau: float = 0.01
-    gamma: float = 0.99 # 0.6
+    gamma: float = 0.6 # 0.6
     batch_size: int = 32
     buffer_capacity: int = 2000
     nb_interval: int = 200  # interval to update target network
+    n_step: int = 1000
 
-    h_short: int = 300   # sliding windows for popularity (Section VI-A)
+    h_short: int = 500   # sliding windows for popularity (Section VI-A)
     h_long: int = 1000
 
     # CPT parameters
@@ -56,16 +58,14 @@ class Config:
     # path_trajectories: str = '/home/eduardo/Workspace/CacheVideoPredict360/Dataset/Trajectories'
 
     ### CERES
-    path_data: str = '/home/es25591/CacheVideoPredict360/Data'
-    path_results: str = '/home/es25591/CacheVideoPredict360/Results'
-    path_trajectories: str = '/home/es25591/CacheVideoPredict360/Dataset/Trajectories'
+    # path_data: str = '/home/es25591/CacheVideoPredict360/Data'
+    # path_results: str = '/home/es25591/CacheVideoPredict360/Results'
+    # path_trajectories: str = '/home/es25591/CacheVideoPredict360/Dataset/Trajectories'
 
-    ### Local Windows
-    # path_data: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Data'
-    # path_results: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Results'
-    # path_trajectories: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Dataset\Trajectories'
-
-
+    ### Local 6G Lab
+    path_data: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Data'
+    path_results: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Results'
+    path_trajectories: str = r'c:\Users\es25591\Workspace\CacheVideoPredict360\Dataset\Trajectories'
 
 
     filename: str = f"drl_dqn_lrdecay{learning_rate_decay}_c{cache_size}_ar{arrival_rate}_z{zipf_alpha}.csv"
